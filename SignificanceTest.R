70258abc6c8c6bd56e92c0824a1ff6c6b94cd19e
razmoudeh <82412080+razmoudeh@users.noreply.github.com>
2022-05-24 11:32:53 -0400

    Create SignificanceTest.R

---
+++ SignificanceTest.R
@@ -0,0 +1,15 @@
+significanceTest <- function(data, tech, col,csvName) {
+  pvals <- list()
+  subsets <- list()
+  for (i in 1:length(data$Subset)) {
+    pval <- wilcox.test(data[data$Subset==data$Subset[i] & data$Superpopulation=="AFR" & data$tech==tech,col], merged[merged$Subset==merged$Subset[i] & merged$Superpopulation!="AFR" & merged$tech==tech,col], alternative = "two.sided")$p.value
+    pvals[[i]]<- pval
+    subsets[[i]]<- merged$Subset[i]
+  }
+  pvals.2 <- c(subsets,pvals)
+  pvalsArray <- array(unlist(pvals.2),dim=c(length(pvals.2)/2,2,1))
+  pValsDf <- as.data.frame(pvalsArray)
+  pValsDf.2 <- pValsDf[order(pValsDf[,2]),]
+  colnames(pValsDf.2) <- c("Subset","pValue")
+  write.csv(pValsDf.2,csvName)
+}
